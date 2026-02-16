c4b5e756553904101a9d22ddb3c3681658e6f12d
Wang Jie <i@i8e.net>
2026-02-17 02:17:11 +0800

    Adds daily log rotation
    Implements daily log rotation using winston-daily-rotate-file to manage log file size and archival, ensuring long-term log availability.
    
    This prevents log files from growing indefinitely, potentially causing disk space issues.

--- src/server/common/logger.ts
+++ src/server/common/logger.ts
@@ -1,6 +1,9 @@
 import os from 'node:os'
-import { createLogger, format } from 'winston'
+import { createLogger, format, transports } from 'winston'
 import ConsoleTransport from 'winston-humanize-console-transport'
+import 'winston-daily-rotate-file'
+import path from 'node:path'
+import process from 'node:process'
 
 const { combine, timestamp } = format
 
@@ -10,3 +13,15 @@ export const logger = createLogger({
   format: combine(timestamp()),
   transports: [new ConsoleTransport()],
 })
+
+const rotateFile = new transports.DailyRotateFile({
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxFiles: '14d',
+  format: format.json(),
+  createSymlink: true,
+  dirname: path.join(process.cwd(), 'logs'),
+  filename: `%DATE%.log`,
+})
+
+logger.add(rotateFile)
